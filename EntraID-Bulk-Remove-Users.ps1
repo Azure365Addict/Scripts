b044ddadc0be6ffdd04c2b36e22601d7082f83ff
a365junkie <78150989+a365junkie@users.noreply.github.com>
2024-10-23 16:35:48 +0200

    Add files via upload

---
+++ EntraID-Bulk-Remove-Users.ps1
@@ -0,0 +1,37 @@
+﻿# Description:
+# Script remove users in bulk.
+#
+# Requirements:
+# - Microsoft.Graph PowerShell module
+# - User.ReadWrite.All permission
+
+# This script is https://github.com/a365junkie/Scripts/EntraID-Bulk-Remove-Users.ps1
+# See https://a365junkie.com/ for more information.
+# V1.0 23-October-2024
+
+# Connect Microsoft Graph
+$Scopes = "User.ReadWrite.All "
+Connect-MgGraph -Scopes $Scopes
+
+# Get the list of users
+$Users = Import-CSV "C:\Temp\EntraID-Bulk-Remove-Users_SAMPLE.csv" 
+
+foreach ($User in $Users)
+
+        {
+        $UPN = $User.UserPrincipalName
+                                                 
+            try
+            {
+                $AADUser = Get-MgUser -UserId $UPN -ErrorAction Stop #Check if user exists
+                Remove-MgUser -UserId $UPN
+                Write-Host "Deleted user $UPN" -ForegroundColor Green
+            }
+    
+                catch
+                {
+                Write-Host "WARNING: Could not delete user $UPN - User does not exist in Entra ID" -ForegroundColor Yellow
+                }
+
+
+        }
