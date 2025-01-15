96ee03a7f2d7f8598d4f19270dc7aa30d2fdef4f
Azure365Addict <78150989+Azure365Addict@users.noreply.github.com>
2025-01-15 10:13:56 +0100

    Update EntraID-Devices-Assign-Extension-Attributes.ps1

--- EntraID-Devices-Assign-Extension-Attributes.ps1
+++ EntraID-Devices-Assign-Extension-Attributes.ps1
@@ -1,12 +1,17 @@
-﻿# Description:
-# Script assigns extension attributes for Entra ID devices.
-#
-# Requirements:
-# - Microsoft.Graph.Beta PowerShell module
-# - Directory.ReadWrite.All and Device.Read.All permissions
+<#
+Description:
+Script assigns extension attributes for Entra ID devices.
 
-# For detailed script execution:: https://github.com/Azure365Addict/Scripts/edit/main/EntraID-Devices-Assign-Extension-Attributes.ps1
-# V1.0 24-January-2025
+Requirements:
+- Microsoft.Graph.Beta PowerShell module
+- Directory.ReadWrite.All and Device.Read.All permissions
+
+Version:
+1.0
+
+More details:
+https://azure365addict.com/2025/01/15/assign-extension-attributes-to-entra-id-devices-using-graph-and-powershell/
+#>
 
 # Connect Microsoft Graph
 $Scopes = "Directory.ReadWrite.All, Device.Read.All"
@@ -21,7 +26,6 @@ $DeviceCountry = "France"
 $DeviceCity = "Paris"
 
 # Assign extensionAttributes
-
 $Attributes = @{
             ExtensionAttributes = @{
                 extensionAttribute1 = $DeviceRegion
@@ -31,8 +35,7 @@ $Attributes = @{
       
              Update-MgBetaDevice -DeviceId $Device.Id -BodyParameter $Attributes
 
-# Clear extensionAttributes
-         
+# Clear extensionAttributes        
 $Attributes = @{
             ExtensionAttributes = @{
                 extensionAttribute1 = ""
@@ -42,4 +45,4 @@ $Attributes = @{
       
 Update-MgBetaDevice -DeviceId $Device.Id -BodyParameter $Attributes
 
-Write-Host `n"*** Please visit " -NoNewline -ForegroundColor Green; Write-Host "azure365addict.com" -ForegroundColor Yellow -NoNewline; Write-Host " to get access to the latest PowerShell related blog entries. ***" -ForegroundColor Green       
+Write-Host `n"*** Please visit " -NoNewline -ForegroundColor Green; Write-Host "azure365addict.com" -ForegroundColor Yellow -NoNewline; Write-Host " to get access to the latest PowerShell related blog entries. ***" -ForegroundColor Green
