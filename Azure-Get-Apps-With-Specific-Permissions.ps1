76948e775b51663e2f6e65e81faecdbb5088c98d
Azure365Addict <78150989+Azure365Addict@users.noreply.github.com>
2025-02-26 12:02:27 +0100

    Add files via upload

---
+++ Azure-Get-Apps-With-Specific-Permissions.ps1
@@ -0,0 +1,20 @@
+﻿Connect-MgGraph -Scopes "Application.Read.All, Directory.Read.All"
+
+$permissionsToCheck = @("EWS.AccessAsUser.All", "full_access_as_app")
+$apps = Get-MgServicePrincipal -All
+$results = @()
+
+foreach ($app in $apps) {
+    $permissions = Get-MgServicePrincipalOauth2PermissionGrant -ServicePrincipalId $app.Id
+    foreach ($permission in $permissions) {
+        if ($permissionsToCheck -contains $permission.Scope) {
+            $results += [PSCustomObject]@{
+                AppName = $app.DisplayName
+                AppId = $app.AppId
+                Permission = $permission.Scope
+            }
+        }
+    }
+}
+
+$results
