ac02dbd82b8bbf60f48c43ef55931dd4ea71335e
Azure365Addict <78150989+Azure365Addict@users.noreply.github.com>
2025-02-26 12:06:00 +0100

    Update Azure-Get-Apps-With-Specific-Permissions.ps1

--- Azure-Get-Apps-With-Specific-Permissions.ps1
+++ Azure-Get-Apps-With-Specific-Permissions.ps1
@@ -1,6 +1,20 @@
-﻿Connect-MgGraph -Scopes "Application.Read.All, Directory.Read.All"
+﻿# Description:
+# Script connects to Microsoft Graph and search for apps with specific permissions
+#
+# Requirements:
+# - Microsoft Graph PowerShell Module
 
+# This script is https://github.com/Azure365Addict/Scripts/edit/main/Azure-Get-Apps-With-Specific-Permissions.ps1
+# See https://365ScriptJunkie.com/ for more information.
+# V1.0 26-February-2025
+
+# Connect to Graph
+Connect-MgGraph -Scopes "Application.Read.All, Directory.Read.All"
+
+# Specify permissions
 $permissionsToCheck = @("EWS.AccessAsUser.All", "full_access_as_app")
+
+# Get all Apps
 $apps = Get-MgServicePrincipal -All
 $results = @()
 
@@ -17,4 +31,5 @@ foreach ($app in $apps) {
     }
 }
 
+# Display results
 $results
